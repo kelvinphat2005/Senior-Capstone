a58da1fb3ae8423af586ffdb725113bb70b7baf1
kelvinphat2005 <kelvinphat12@gmail.com>
2023-03-05 20:54:09 -0800

    reworked HUD

---
+++ assets/hud/Holding.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+@onready var player
+@onready var inventory 
+
+var player_in_scene = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
