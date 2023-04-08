7cc0bd23ce12ff234756fe38c4dca4d2a1933ce1
kelvinphat2005 <kelvinphat12@gmail.com>
2023-04-08 16:24:42 -0700

    level 1 reimport

--- player/Player Camera.gd
+++ player/Player Camera.gd
@@ -3,9 +3,9 @@ extends Node3D
 var READY = false
 
 # the radius area that the player can be before the camera starts to follow
-@export var inner_cam_thresh = 8
+@export var inner_cam_thresh = 0
 # the radius the player can be before the camera completely stops
-@export var outer_cam_thresh = 24
+@export var outer_cam_thresh = 10
 
 # cam offsets
 @export var z_offset = 0
@@ -32,18 +32,24 @@ func _ready():
 
 		# sets camera position
 		# z is how far behind the cam is from player
-		transform.origin = Vector3(player_pos.x,12+y_offset,player_pos.z+8+z_offset)
-		rotation_degrees = Vector3(-45, 0, 0) 
+		# transform.origin = Vector3(player_pos.x,y_offset+4,player_pos.z+z_offset-2)
+		rotation_degrees = Vector3(-60, 0, 0) 
 		
 		# creates origin point
 		# inner and outer cam thresholds are based on this
 		var origin = Vector3(position.x,0,position.z)
 
 func _process(delta):
+	player_pos = player.position
+	position = (Vector3(player_pos.x,8,player_pos.z+z_offset+3))
+	print(position)
+	'''player_pos = player.position
 	# print(self.position)
 	if READY:
 		# camera movemeent
-		# print(player.position.z - origin.z)
+		print(origin.z)
+		print(player_pos.z - origin.z)
+		print(in_radius_z)
 		if in_radius_x:
 			if player.position.x - origin.x > inner_cam_thresh:
 				translate(Vector3(player.velocity.x * delta,0,0))
@@ -64,11 +70,11 @@ func _process(delta):
 			if player.position.z - origin.z > inner_cam_thresh:
 				translate(Vector3(0,0,0))
 				in_radius_z = false
-			elif player.position.z - origin.z < inner_cam_thresh * -1:
+			elif player.position.z - origin.z < inner_cam_thresh:
 				translate(Vector3(0,player.velocity.z * delta * -1,player.velocity.z * delta))
 				in_radius_z = false
 		else:
 			if player.position.z - origin.z < outer_cam_thresh and player.position.z > inner_cam_thresh:
 				in_radius_z = true
-			elif player.position.z - origin.x > outer_cam_thresh * -1 and player.position.z < inner_cam_thresh * -1:
-				in_radius_z = true
+			elif player.position.z - origin.z > outer_cam_thresh * -1 and player.position.z < inner_cam_thresh * -1:
+				in_radius_z = true'''
