7a94522fa9419c6d8a30b65ee5b75d7f0c8e6eae
kelvinphat2005 <kelvinphat12@gmail.com>
2023-05-13 09:55:03 -0700

    unnessary commit #1943

---
+++ levels/reception/reception_playable.gd
@@ -0,0 +1,8 @@
+extends Node
+
+@onready var player = $Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var quest_handler = player.get_node("Quest Handler")
+	quest_handler.add_quest(3)
--- levels/starting level/2 Fill Cup.gd
+++ levels/starting level/2 Fill Cup.gd
@@ -2,9 +2,9 @@ extends Event
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	event_id = 2
-	active = false
-	result_quest = [3]
-	completed_quest = 2
+	event_id = 7
+	active = true
+	result_quest = [4]
+	completed_quest = 3
 	trigger = TRIGGER.INTERACT
 	result = [RESULT.QUEST_COMPLETE, RESULT.QUEST_ADD]
